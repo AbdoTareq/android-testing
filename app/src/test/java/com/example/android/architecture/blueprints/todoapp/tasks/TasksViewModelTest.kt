d9b6654dd195e14396d5a173decefa1c38ae7c47
Abdo Tareq <abdotarekfathy@gmail.com>
2020-05-15 17:48:19 +0200

    feat: Add live data test

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,12 +1,24 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.IsNot.not
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class TasksViewModelTest{
+class TasksViewModelTest {
+
+    // 2 things needed to test live data (1. rule & 2. Observe that live data)
+
+    // 1st rule
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
     @Test
     fun addNewTask_setsNewTaskEvent() {
@@ -16,8 +28,12 @@ class TasksViewModelTest{
         // When adding a new task
         tasksViewModel.addNewTask()
 
+        // 2nd Observe that live data
         // Then the new task event is triggered
-
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(
+                value.getContentIfNotHandled(), (not(nullValue()))
+        )
     }
 
 }
